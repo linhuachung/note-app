641eb3b1d25f2c64f95c8ac4ed99eb77b989ad73
Chun <chunglh@bap.jp>
2023-03-14 14:25:52 +0700

    done for deloy 7

--- client/src/utils/constants.jsx
+++ client/src/utils/constants.jsx
@@ -1,2 +1,2 @@
 export const GRAPHQL_SERVER = 'https://my-note-app-server.onrender.com'
-export const GRAPHQL_SUBSCRIPTION_ENDPOINT = 'ws://my-note-app-server.onrender.com'
+export const GRAPHQL_SUBSCRIPTION_ENDPOINT = 'ws://my-note-app-server.onrender.com/'
